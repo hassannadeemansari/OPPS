468ddb93907fc3934f5561cb1bd698c3525a71bf
Hassan Nadeem <164927940+hassannadeemansari@users.noreply.github.com>
2025-05-21 23:39:13 +0500

    deposit , withdraw cash and check balance

---
+++ second.py
@@ -0,0 +1,23 @@
+
+
+class Bank():
+    def __init__(self , balance = 0 ):
+        self.balance = balance
+
+    def Checkbalance(self):
+        return f"your current balance is {self.balance}"
+    
+    def Deposit(self , Amount):
+
+            return f"your  balnce is {self.balance == Amount + self.balance} after you deposit {Amount}"
+    
+    def Witdraw(self , Amount):
+         return f"your balance is {self.balance - Amount} after you withdraw {Amount}"
+    
+
+Person = Bank(500)
+print(Person.Checkbalance())
+print(Person.Deposit(1000))
+print(Person.Checkbalance())
+print(Person.Witdraw(200))
+print(Person)
